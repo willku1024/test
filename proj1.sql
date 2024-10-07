b11f5a6a463cd24ece7e2e07e957323fc1c92ad0
willku <willku2012@gmail.com>
2024-10-07 15:07:48 +1100

    init

---
+++ proj1.sql
@@ -0,0 +1,79 @@
+-------------------------
+-- Project 1 Solution Template
+-- COMP9311 24T3
+-- Name: xxx
+-- zID: zzz
+-------------------------
+
+
+-- Q1
+DROP VIEW IF EXISTS Q1 CASCADE;
+CREATE or REPLACE VIEW Q1(count) AS
+-- SQL query
+;
+
+
+
+-- Q2
+DROP VIEW IF EXISTS Q2 CASCADE;
+CREATE or REPLACE VIEW Q2(count) AS
+-- SQL query
+;
+
+
+
+-- Q3
+DROP VIEW IF EXISTS Q3 CASCADE;
+CREATE or REPLACE VIEW Q3(unswid,name) AS
+-- SQL query
+;
+
+-- Q4
+DROP VIEW IF EXISTS Q4 CASCADE;
+CREATE or REPLACE VIEW Q4(unswid,name) AS
+-- SQL query
+;
+
+-- Q5
+DROP VIEW IF EXISTS Q5 CASCADE;
+CREATE or REPLACE VIEW Q5(count) AS
+-- SQL query
+;
+
+
+-- Q6
+DROP VIEW IF EXISTS Q6 CASCADE;
+CREATE or REPLACE VIEW Q6(count) AS
+-- SQL query
+;
+
+-- Q7
+DROP VIEW IF EXISTS Q7 CASCADE;
+CREATE or REPLACE VIEW Q7(course_id,unswid) AS
+-- SQL query
+;
+
+
+
+-- Q8
+DROP VIEW IF EXISTS Q8 CASCADE;
+CREATE or REPLACE VIEW Q8(course_id,unswid) AS
+-- SQL query
+;
+
+
+
+-- Q9
+DROP FUNCTION IF EXISTS Q9 CASCADE;
+CREATE or REPLACE FUNCTION Q9(subject1 integer, subject2 integer) returns text
+as $$
+--Function body
+$$ language plpgsql;
+
+
+-- Q10
+DROP FUNCTION IF EXISTS Q10 CASCADE;
+CREATE or REPLACE FUNCTION Q10(subject1 integer, subject2 integer) returns text
+as $$
+--Function body
+$$ language plpgsql;
